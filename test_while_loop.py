cbff30ad32819927447d0850f91857adb22642df
Ben Woodfield <raser.13@hotmail.com>
2016-10-16 00:38:16 +0000

    Simple loop set-up

---
+++ test_while_loop.py
@@ -0,0 +1,14 @@
+#!usr/bin/env/python
+# Ben Woodfield
+# A basic demonstration of a loop
+
+sum = 0
+i = 0
+while i < 10:
+	sum = sum + i
+	print sum
+	i = i + 1
+	
+for i in range(10):
+	sum = sum + i
+	print sum
