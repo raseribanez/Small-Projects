9c8021abe2f0d83d49e34b34f5e367c11772a979
Ben Woodfield <raser.13@hotmail.com>
2016-11-01 22:34:53 +0000

    Add files via upload

---
+++ if_elif_else_example.py
@@ -0,0 +1,11 @@
+# Ben Woodfield
+# An example of an if - elif - else statement
+
+x = 1
+
+if x < 0:
+    print "x is negative"
+elif x % 2:
+    print "x is positive and odd"
+else:
+    print "x is even and non negative"
