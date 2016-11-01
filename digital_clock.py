0869a91f707ef6ac6adc16ba42d03b3c8cfc2e24
Ben Woodfield <raser.13@hotmail.com>
2016-11-01 22:29:12 +0000

    a simple digital clock

---
+++ digital_clock.py
@@ -0,0 +1,19 @@
+# Ben Woodfield
+# Digital Clock
+from Tkinter import *
+import Tkinter
+import time
+
+curtime = ''
+clock = Tkinter.Label()
+clock.pack()
+
+def tick():
+    global curtime
+    newtime = time.strftime('%H:%M:%S')
+    if newtime != curtime:
+        curtime = newtime
+        clock.config(text=curtime)
+    clock.after(200, tick)
+tick()
+clock.mainloop()
