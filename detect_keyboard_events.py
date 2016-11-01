809b6fed82513941f724f66698e3fda69ec17fd5
Ben Woodfield <raser.13@hotmail.com>
2016-11-01 23:01:33 +0000

    a cool little program that detects keyboard events

---
+++ detect_keyboard_events.py
@@ -0,0 +1,36 @@
+# Ben Woodfield
+# A simple GUI program that detects Keyboard activity and returns to the user
+# uses bind_all
+# UnHash to add mouse activity
+
+import Tkinter
+from Tkinter import *
+
+top = Tk()
+top.minsize(300,100)
+top.title('Detect Key Press')
+prompt = 'Press any Key, Number or Symbol'
+L = Label(top, text=prompt, width=len(prompt))
+L.pack()
+
+
+def key(event):
+    if event.char == event.keysym:
+        msg = 'Normal Key %r' % event.char
+    elif len(event.char)==1:
+        msg = 'Punctuation Key %r (%r)' % (event.keysym, event.char)
+    else:
+        msg = 'Special Key %r' % event.keysym
+    L.config(text=msg)
+L.bind_all('<Key>', key)
+
+#def do_mouse(event_name):
+#    def mouse_binding(event):
+#        L.config(text='Mouse event %s' % eventname)
+#    L.bind_all('<%s>' %eventname, mouse_binding)
+#for i in range(1,4):
+#    do_mouse('Button-%s'%i)
+#    do_mouse('ButtonRelease-%s'%i)
+#    do_mouse('Double-Button-%s'%i)
+
+top.mainloop()
