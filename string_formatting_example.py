bf9da1061e911e6b69627adea556479003874571
Ben Woodfield <raser.13@hotmail.com>
2016-11-02 20:11:28 +0000

    added some educational examples

---
+++ string_formatting_example.py
@@ -0,0 +1,9 @@
+# Ben Woodfield
+# String Formatting Example
+
+x = 42
+y = 3.14
+z = "George"
+print 'result = %d' %x              # prints: result = 42
+print 'answers are: %d %f' %(x,y)   # prints: answers are: 42 3.14
+print 'hello %s' %z                 # prints: hello george
