5078198b7dbaa90b75f66e95002957279692bf12
Ben Woodfield <raser.13@hotmail.com>
2016-11-05 13:42:29 +0000

    this version asks the user for a value

---
+++ celsius_to_farenheit_user_input.py
@@ -0,0 +1,12 @@
+eld
+# Convert celsius to farenheit
+
+# change this value for a different result
+#celsius = 37.5
+
+# Use this method for user input values
+celsius = int(input("Enter your Value in Celsius: "))
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree
