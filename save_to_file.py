c0b7867cb99b1d826d3ebc5e341eeb237f1d999b
Ben Woodfield <raser.13@hotmail.com>
2016-11-08 19:28:00 +0000

    added to code

--- save_to_file.py
+++ save_to_file.py
@@ -1,4 +1,8 @@
-# Write to a text file
+# Ben Woodfield
+# A basic script that writes data to a text file
+# Text example shown...uncomment line 7 to use this in a different context 
+
 f = open( 'file.py', 'w' )
-f.write( 'dict = ' + repr(dict) + '\n' )
+f.write('Heres your saved text!')
+#f.write( 'dict = ' + repr(dict) + '\n' )
 f.close()
