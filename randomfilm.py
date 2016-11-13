f3126330916ef8080cb6ecbf7563a1d4cd8934bc
Ben Woodfield <raser.13@hotmail.com>
2016-11-13 15:53:09 +0000

    picks a random film-name from a list

---
+++ randomfilm.py
@@ -0,0 +1,13 @@
+# Ben Woodfield
+
+# This was a little program I came across while doing basic tutorials
+# It basically picks a random choice from a list of strings/name
+# To use just replace the films in the list for your own
+
+# random film picker - for undecided choice of DVD
+
+import random
+
+list = ['Matrix One', 'Soldier', 'Pitch Black', 'Chronicles of Riddick', 'i Robot', 'Ghost Dog', 'Alien', 'Star-Trek - Into the Darkness', 'Promethius', 'Guardians of the Galaxy']
+
+print(random.choice(list))
