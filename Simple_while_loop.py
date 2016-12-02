051ba3408388e9d84937b9ba96d877a707f444a1
Ben Woodfield <raser.13@hotmail.com>
2016-12-02 16:08:46 +0000

    Update Simple_while_loop.py

--- Simple_while_loop.py
+++ Simple_while_loop.py
@@ -1,6 +1,11 @@
 #!usr/bin/env/python
 # Ben Woodfield
 # A basic demonstration of a loop
+# This was from a guide I was following - I am not sure what the output
+# is actually doing, theres no obvious pattern, it starts off by increasing
+# at an 'increase + 1' type rate (increase by 5, then by 6, then by 7....)
+# But that stops when it reaches 45 and it goes off. I just wanted some 
+# basic examples to use as a reference if I geet stuck.
 
 sum = 0
 i = 0
