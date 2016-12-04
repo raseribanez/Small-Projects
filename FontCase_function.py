434036d9edc265d2cbbfb12587da8cfa365ab326
Ben Woodfield <raser.13@hotmail.com>
2016-12-04 17:40:14 +0000

    added upper/lower case functions

---
+++ FontCase_function.py
@@ -0,0 +1,18 @@
+# Ben woodfield
+# After playing around with upper() and lower()
+# I put this into a little function
+# Run the code, then use arguments in the shell to use either function
+
+# Lowercase function
+def lowercase(string):
+  word = string.lower()
+  return word
+
+# Uppercase function
+def uppercase(string):
+    word = string.upper()
+    return word
+
+# use this with:
+#lowercase("WORD")
+#uppercase("word")
